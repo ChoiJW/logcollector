a58c092d462bca4ad6df15724f8a591c02c72bf1
supportchoi <supportchoi@hotmail.com>
2018-02-19 02:54:00 +0900

    netty -> simplesocketserver 변경

--- src/main/java/logcollector/LogServer.java
+++ src/main/java/logcollector/LogServer.java
@@ -1,49 +1,20 @@
 package logcollector;
 
-import org.apache.log4j.*;
+import java.io.File;
 
-import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
-import io.netty.channel.EventLoopGroup;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.apache.log4j.*;
+import org.apache.log4j.net.*;
 
 public class LogServer {
 
-	//private final Logger logger = Logger.getLogger(LogServer.class);
+	private final Logger logger = Logger.getLogger(LogServer.class);
 	
 	public static void main(String[] args) throws Exception {
-
-		//LogServer logServer = new LogServer();
+		System.out.println(System.getProperty("user.dir"));
 		
-		EventLoopGroup bossGroup = new NioEventLoopGroup(1);
-		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		String[] arguments = {"4560", System.getProperty("user.dir") + File.separator + "Config/log4j-server.properties"};  
 
-		try {
-			
-			ServerBootstrap b = new ServerBootstrap();
-			
-			b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
-					.childHandler(new ChannelInitializer<SocketChannel>() {
-						@Override
-						protected void initChannel(SocketChannel ch) {
-							ChannelPipeline p = ch.pipeline();
-							p.addLast(new LogHandler());
-						}
-					});
-
-			ChannelFuture f = b.bind(4560).sync();
-			f.channel().closeFuture().sync();
-			
-			//logServer.logger.debug("test log test log test log test log test log");
-			
-		} finally {
-			workerGroup.shutdownGracefully();
-			bossGroup.shutdownGracefully();
-		}
+        SimpleSocketServer.main(arguments);  
 
 	}
 }
